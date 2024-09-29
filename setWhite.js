1169a45e0dda70e1daab572291244f1cdb5564e9
Mingqi Li <mingqi_li@icloud.com>
2024-09-29 12:32:24 +0200

    add black king, move absorption

--- setWhite.js
+++ setWhite.js
@@ -4,36 +4,44 @@
     move[1][0] is the x coordinate of the second click = finalx
     move[1][1] is the y coordinate of the second click = finaly
 */
+//return = 0: all ok
+//return = 1: piece goes on same field as before
+//return = 2: own piece in the way
+//return = 3: piece capture/go on the field of their pieces
 
 function WhitePiecesCalculationMoves(clientmove, backendboard) {
     console.log(clientmove[0][0]);
-    initialx = clientmove[0][0];
-    initialy = clientmove[0][1];
-    finalx = clientmove[1][0];
-    finaly = clientmove[1][1];
-    switch (backendboard[initialx][initialy]) {
+    initialy = clientmove[0][0];
+    initialx = clientmove[0][1];
+    finaly = clientmove[1][0];
+    finalx = clientmove[1][1];
+    switch (backendboard[initialy][initialx]) {
         case 1: //pawn
-            console.log("pawn clicked");
             //checking if two coordinates have the same x-coordinate
             if (initialx == finalx) {
                 //checks if piece y coordinate was changed
                 if (initialy == finaly) {
                     console.log("piece can't move to same position as before")
                     return 1;
-                } else
+                } else {
                     //this one checks how many moves to the front the pawn makes
                     //if one move is made -> move
                     //if two moves are made and pawn is on second rank -> move
-                    if (initialy - 1 == finaly || (initialy == 6 && initialy - 2 == finaly)) {
+                    if (initialy + 1 == finaly || (initialy == 1 && initialy + 2 == finaly)) {
+                        if (backendboard[finaly][initialx] > 0){
+                            console.log("piece cant capture/go on the field of their pieces");
+                            return 3;
+                        } else {
                         //now we change the board data in the backend and the frontend
                         serverPostToClient(clientmove);
                         return 0;
+                        }
                     }
+                }
             }
             break;
 
         case 2: //knight
-            console.log("knight clicked");
             if (((initialx == finalx - 1 || initialx == finalx + 1) && (initialy == finaly + 2 || initialy == finaly - 2)) || ((initialx == finalx - 2 || initialx == finalx + 2) && (initialy == finaly + 1 || initialy == finaly - 1))) {
                 //if the programm comes this far, it means that the move is possible so we play it
                 //check if piece moves to the same position as before
@@ -41,8 +49,14 @@ function WhitePiecesCalculationMoves(clientmove, backendboard) {
                     console.log("piece can't move to same position as before");
                     return 1;
                 } else {
+                    if (backendboard[finaly][initialx] > 0){
+                        console.log("piece cant capture/go on the field of their pieces");
+                        return 3;
+                    } else {
+                    //now we change the board data in the backend and the frontend
                     serverPostToClient(clientmove);
                     return 0;
+                    }
                 }
             }
             break;
@@ -53,8 +67,14 @@ function WhitePiecesCalculationMoves(clientmove, backendboard) {
                     console.log("piece can't move to same position as before")
                     return 1;
                 } else {
-                    serverPostToClient(clientmove)
+                    if (backendboard[finaly][initialx] > 0){
+                        console.log("piece cant capture/go on the field of their pieces");
+                        return 3;
+                    } else {
+                    //now we change the board data in the backend and the frontend
+                    serverPostToClient(clientmove);
                     return 0;
+                    }
                 }
             }
             break;
@@ -65,8 +85,14 @@ function WhitePiecesCalculationMoves(clientmove, backendboard) {
                     console.log("piece can't move to same position as before")
                     return 1;
                 } else {
-                    serverPostToClient(clientmove)
+                    if (backendboard[finaly][initialx] > 0){
+                        console.log("piece cant capture/go on the field of their pieces");
+                        return 3;
+                    } else {
+                    //now we change the board data in the backend and the frontend
+                    serverPostToClient(clientmove);
                     return 0;
+                    }
                 }
             }
             break;
@@ -77,8 +103,14 @@ function WhitePiecesCalculationMoves(clientmove, backendboard) {
                     console.log("piece can't move to same position as before")
                     return 1;
                 } else {
-                    serverPostToClient(clientmove)
+                    if (backendboard[finaly][initialx] > 0){
+                        console.log("piece cant capture/go on the field of their pieces");
+                        return 3;
+                    } else {
+                    //now we change the board data in the backend and the frontend
+                    serverPostToClient(clientmove);
                     return 0;
+                    }
                 }
             }
             break;
@@ -108,14 +140,20 @@ function WhitePiecesCalculationMoves(clientmove, backendboard) {
                             }
                         }
                     }
+                    if (backendboard[finaly][initialx] > 0){
+                        console.log("piece cant capture/go on the field of their pieces");
+                        return 3;
+                    } else {
+                    //now we change the board data in the backend and the frontend
                     serverPostToClient(clientmove);
                     return 0;
+                    }
                 }
             }
             break;
 
-            default:
-                console.log("invalid move");
+        default:
+            console.log("invalid move");
     }
 
 }
